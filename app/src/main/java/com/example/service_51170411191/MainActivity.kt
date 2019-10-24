5874167bebd07876f816df8dc4c6fec69175b992
Revaalfian <lolamonda10@gmail.com>
2019-10-24 13:18:23 +0700

    service91

---
+++ app/src/main/java/com/example/service_51170411191/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.service_51170411191
+
+import android.media.MediaPlayer
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_fb.setOnClickListener({
+            val i = Intent(Intent.ACTION_VIEW,
+                Uri.parse("http://www.facebook.com/langitinspirasi"))
+                startActivity(i)
+        })
+        btn_ig.setOnClickListener({
+            val i = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("http://www.instagram.com/langitinspirasi"))
+            startActivity(i)
+        })
+        var MediaPlayer: MediaPlayer? = MediaPlayer.create(this,R.raw.dalu)
+            btn_play.setOnClickListener{
+                MediaPlayer?.start()
+            }
+        btn_pause.setOnClickListener{
+            MediaPlayer?.pause()
+            }
+        btn_stop.setOnClickListener{
+            MediaPlayer?.stop()
+            MediaPlayer?.seekTo(0)
+            }
+        }
+}
